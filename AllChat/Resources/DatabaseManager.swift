c252323e60093ccae13e026a36bd98156c5dc160
AhmetIOS <105635443+AhmetIOS@users.noreply.github.com>
2022-08-22 00:31:52 +0300

    Github Problem

--- AllChat/Resources/DatabaseManager.swift
+++ AllChat/Resources/DatabaseManager.swift
@@ -17,7 +17,11 @@ final class DatabaseManager {
 extension DatabaseManager {
         public func userExists(with email: String,
                                completion: @escaping ((Bool) -> Void)) {
-            database.child(email).observeSingleEvent(of: .value) { snapShot in
+            
+            var safeemail = email.replacingOccurrences(of: ".", with: "-")
+            safeemail = safeemail.replacingOccurrences(of: "@", with: "-")
+            
+            database.child(safeemail).observeSingleEvent(of: .value) { snapShot in
                 guard snapShot.value as? String != nil else {
                     completion(false)
                     return
@@ -29,7 +33,7 @@ extension DatabaseManager {
         /// inserts new users to database
         public func insertUser(with user: ChatAppUser) {
             
-            database.child(user.emailAddress).setValue([
+            database.child(user.safeEmail).setValue([
                 "first_name": user.firstName,
                 "last_name": user.lastName
             ])
@@ -42,5 +46,11 @@ extension DatabaseManager {
         let lastName: String
         let emailAddress: String
 //        let profilePictureUrl: String
+        
+        var safeEmail: String {
+            var safeemail = emailAddress.replacingOccurrences(of: ".", with: "-")
+            safeemail = safeemail.replacingOccurrences(of: "@", with: "-")
+            return safeemail
+        }
     }
 }
